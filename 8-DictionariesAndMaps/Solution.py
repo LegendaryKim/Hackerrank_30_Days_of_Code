42a26046b6b61d06cbc809fde57c918be81a8652
HwanpyoKim <khwanpy1@jhu.edu>
2017-12-06 16:23:12 -0500

    Solve Python - 8-DictionariesAndMaps

--- 8-DictionariesAndMaps/Solution.py
+++ 8-DictionariesAndMaps/Solution.py
@@ -2,17 +2,22 @@ num = int(input())
 
 phone_book = {}
 for i in range(num):
-	entry = str(input()).split(" ")
+    entry = str(input()).split(" ")
 
-	name = entry[0]
-	phone = int(entry[1])
-	phone_book[name] = phone
+    name = entry[0]
+    phone = int(entry[1])
+    phone_book[name] = phone
 
-for j in range(num):
-	name = str(input())
+while True:
+    try:
+        name = input()
 
-	if name in phone_book:
-		phone = phone_book[name]
-		print(name + "=" + str(phone))
-	else:
-		print("Not found")
+        if name in phone_book:
+            phone = phone_book[name]
+            print(name + "=" + str(phone))
+        else:
+            print("Not found")
+    except:
+        break
+        
+
