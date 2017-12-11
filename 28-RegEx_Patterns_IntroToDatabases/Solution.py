ef40f0622f09cad33dd5ea9b7d632c69f5a51e38
HwanpyoKim <khwanpy1@jhu.edu>
2017-12-11 18:11:22 -0500

    Solve Python - 28-RegEx_Patterns_IntroToDatabases

---
+++ 28-RegEx_Patterns_IntroToDatabases/Solution.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+import re
+import sys
+
+Names = []
+N = int(input().strip())
+for a0 in range(N):
+    firstName,emailID = input().strip().split(' ')
+    firstName,emailID = [str(firstName),str(emailID)]
+    if re.search("@gmail\.com$", emailID):
+        Names.append(firstName)
+
+Names.sort()
+
+for name in Names:
+    print(name)
+
+
