06b7d2247afe08e6ddbdfd299b2546d6c163af03
HwanpyoKim <khwanpy1@jhu.edu>
2017-12-11 17:08:38 -0500

    Solve C++ - 27-Testing

---
+++ 27-Testing/Solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << '5' << endl;
+    cout << "4 3" << endl;
+    cout << "-1 -3 -1 2" << endl;
+    cout << "5 2" << endl;
+    cout << "0 -1 2 1 4" << endl;
+    cout << "3 1" << endl;
+    cout << "-1 0 -1" << endl;
+    cout << "4 2" << endl;
+    cout << "0 1 2 3" << endl;
+    cout << "4 3" << endl;
+    cout << "-1 -4" << endl;
+    
+    return 0;
+}
